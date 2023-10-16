b742a5eb54879269dc153c9ad82958c100db3688
aniruddh02 <aniruddh02@gmail.com>
2023-10-16 18:07:44 -0500

    First commit

---
+++ s2s-api/src/main/java/com/s2s/s2sapi/S2sApiApplication.java
@@ -0,0 +1,13 @@
+package com.s2s.s2sapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2sApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2sApiApplication.class, args);
+	}
+
+}
---
+++ s2s-api/src/test/java/com/s2s/s2sapi/S2sApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s2s.s2sapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2sApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
